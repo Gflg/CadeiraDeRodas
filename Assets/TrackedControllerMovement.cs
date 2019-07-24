0da439966e753841838078bd370992f530778881
UffMediaLab <emailmedialab@gmail.com>
2019-07-24 10:11:06 -0300

    Se movimentando com a cadeira de rodas no HTC Vive

--- Assets/TrackedControllerMovement.cs
+++ Assets/TrackedControllerMovement.cs
@@ -8,11 +8,15 @@ public class TrackedControllerMovement : MonoBehaviour
     public Transform rig;
     [SerializeField]
     public Transform head;
+    [SerializeField]
+    public Transform wheelchair;
 
     private SteamVR_Controller.Device device { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
     private SteamVR_TrackedObject trackedObj;
     private Vector3[] contPos;
     private int i;
+
+    private Vector3 diferenca;
 
     private void Start()
     {
@@ -21,31 +25,36 @@ public class TrackedControllerMovement : MonoBehaviour
         contPos[0] = Vector3.zero;
         contPos[1] = Vector3.zero;
         i = 1;
+        diferenca = rig.position - wheelchair.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (device.GetPress(Valve.VR.EVRButtonId.k_EButton_Grip))
+        Debug.Log(diferenca);
+        if (device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
         {
             contPos[i] = transform.position;
             i++;
             if (i == 2)
                 i = 0;
 
-            if (device.velocity.sqrMagnitude > 1)
+            Debug.Log(i);
+            if (device.velocity.sqrMagnitude > 0.08)
             {
                 Vector3 dir = contPos[1] - contPos[0];
                 if (dir.y < 0 && dir.z < 0 || dir.y > 0 && dir.z > 0)
                 {
-                    rig.position = rig.position + head.forward * device.velocity.magnitude * Time.deltaTime;
+                    rig.position = rig.position + head.forward * (3/2) * device.velocity.magnitude * Time.deltaTime;
+                    wheelchair.position = rig.position - diferenca;
                 }
                 else
                 {
                     dir = contPos[0] - contPos[1];
                     if(dir.y > 0 && dir.z > 0 || dir.y < 0 && dir.z < 0)
                     {
-                        rig.position = rig.position + head.forward * device.velocity.magnitude * Time.deltaTime;
+                        rig.position = rig.position + head.forward * (3/2) * device.velocity.magnitude * Time.deltaTime;
+                        wheelchair.position = rig.position - diferenca;
                     }
                 }
 
